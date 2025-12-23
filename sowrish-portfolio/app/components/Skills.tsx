2ad7f48b83864a07a3578b454a5224a3285b44cb
Sowrish Sai Surapaneni <sowrish.10@icloud.com>
2025-12-23 15:33:48 -0600

    Update resume with portfolio link to test CI/CD

--- sowrish-portfolio/app/components/Skills.tsx
+++ sowrish-portfolio/app/components/Skills.tsx
@@ -55,7 +55,7 @@ export default function Skills() {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
-                    <span className="font-semibold">Pursuing AWS Solutions Engineer Certification</span>
+                    <span className="font-semibold">Pursuing AWS Certified Solutions Architect - Associate Certification</span>
                   </div>
                 </div>
               )}
